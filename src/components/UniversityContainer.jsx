dbfea5c274baab24a9f803f70eda4144a5d13886
Krupal Patel <krupalgp2003@gmail.com>
2024-08-22 16:52:10 +0530

    naming changes

---
+++ src/components/UniversityContainer.jsx
@@ -0,0 +1,6 @@
+const UniversityContainer = () => {
+  return (
+    <section className="mt-8 max-w-[1200px] w-full py-8 grid grid-cols-3 gap-4 rounded-md bg-[#171515]"></section>
+  );
+};
+export default UniversityContainer;
