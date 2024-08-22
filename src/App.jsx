d8e5cfd3d9c02f6da9186e9ca8ba1a145bcbd264
Krupal Patel <krupalgp2003@gmail.com>
2024-08-22 16:09:12 +0530

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <>
       <div className="w-dvw h-dvh bg-[#202020] flex items-center flex-col p-2">
         <Navbar />
+        <Hero />
+        <Search />
       </div>
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className="w-full m-36 flex flex-col items-center">
+      <p className="text-5xl font-bold">
+        Welcome to <span className="text-[#f56464]">University</span> Asylum
+      </p>
+      <p className="text-xl pt-4">All University at one place</p>
+    </section>
+  );
+};
+export default Hero;
