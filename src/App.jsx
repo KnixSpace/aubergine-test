ec153bc6fa0b65933a093824b6bfdccc3f9e6443
Krupal Patel <krupalgp2003@gmail.com>
2024-08-22 16:04:20 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>Infinix is alive</div>;
+  return (
+    <>
+      <div className="w-dvw h-dvh bg-[#202020] flex items-center flex-col p-2">
+        <Navbar />
+      </div>
+    </>
+  );
 };
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+      <section className="mycontainer">
+        <nav className="py-4 flex justify-center font-medium text-2xl">
+          University Asylum
+        </nav>
+      </section>
+    );
+  };
+  export default Navbar;
